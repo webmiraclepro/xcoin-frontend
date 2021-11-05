ba6a0b386c163621242b4af651c739247ce95b1f
webmiraclepro <webmiraclepro@gmail.com>
2021-11-05 19:57:05 +0800

    [impl] gradient-animated background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-function App() {
+import styled from 'styled-components';
+import { keyframes } from 'styled-components'
+
+const gradientAnimation = keyframes`
+  0% {
+    background-position: 0% 50%
+  }
+  50% {
+    background-position: 100% 50%
+  }
+  100% {
+    background-position: 0% 50%
+  }
+`;
+
+const AnimatedBackground = styled.div`
+  width: 100wh;
+  height: 100vh;
+  color: #fff;
+  background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
+  background-size: 400% 400%;
+  -webkit-animation: ${gradientAnimation} 10s ease infinite;
+  -moz-animation: ${gradientAnimation} 10s ease infinite;
+  animation: ${gradientAnimation} 10s ease infinite;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <h3>Simple App</h3>
-    </div>
+    <AnimatedBackground />
   );
-}
+};
 
 export default App;
